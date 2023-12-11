b089bf2bfc2191661dd3929413c9e311e0d26236
tonto2423 <tnt.kosen2423@gmail.com>
2023-12-11 22:09:50 +0900

    feat: オドメトリのコードを追加

---
+++ src/odometry_class.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+#include "odometry_class.hpp"
+
+using namespace std;
+
+deltaPose2D Odometry::calc_vel(double _tire_angle[3]) {
+    // 速度の計算
+    int delta_angle[3];
+    for (int i=0; i < 3; i++) {
+        // カウントの差分から計測輪の微小変位を計算
+        delta_angle[i] = _tire_angle[i] - tire_angle[i];
+    }
+    deltaPose2D delta_pose;
+    double delta_x = radius * 2.0 / 3.0 * (-delta_angle[0] + delta_angle[1] / 2.0 + delta_counts[2] / 2.0);
+    double delta_y = radius * 2.0 / 3.0 * (-delta_angle[1] + delta_angle[2]) * 0.866025403784;
+    // ロボットの向きに合わせて速度ベクトルを回転
+    delta_pose.x = delta_x * cos(pose.theta) - delta_y * sin(pose.theta);
+    delta_pose.y = delta_x * sin(pose.theta) + delta_y * cos(pose.theta);
+    delta_pose.theta = radius * (delta_angle[0] + delta_angle[1] + delta_angle[2]) / 3.0 / length;
+    update_angles(_tire_angle); // 前回カウントの更新
+    return delta_pose;
+}
+
+void Odometry::calc_pose(deltaPose2D delta_pose) {
+    // 姿勢の積分
+    pose.x += delta_pose.x;
+    pose.y += delta_pose.y;
+    pose.theta += delta_pose.theta;
+    // pose.theta = fmod(pose.theta + M_PI, 2 * M_PI) - M_PI;
+}
+
+void Odometry::update_angles(double _tire_angle[3]) {
+    // 前回のタイヤ角変位を更新
+    for (int i=0; i<3; i++) {
+        tire_angle[i] = _tire_angle[i];
+    }
+}
+
+void Odometry::update_pose(double _tire_angle[3]) {
+    // 位置の更新
+    calc_pose(calc_vel(_tire_angle));
+}
+
+void Odometry::set_pose(pose2D pose0) {
+    // 姿勢をセットする
+    pose.x = pose0.x;
+    pose.y = pose0.y;
+    pose.theta = pose0.theta;
+}
+
+pose2D Odometry::get_pose() {
+    return pose;
+}
