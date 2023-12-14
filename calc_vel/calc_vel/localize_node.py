0b7ff87c6b34488ef46228f5c2f968d36ae2da1a
tonto2423 <tnt.kosen2423@gmail.com>
2023-12-14 18:47:00 +0900

    PoseをPoseStampedに変更

--- calc_vel/calc_vel/localize_node.py
+++ calc_vel/calc_vel/localize_node.py
@@ -1,31 +1,31 @@
 # description: 仮の自己位置計算ノード
-# input: odometry_poseトピック(geometry_msgs/msg/Pose型)
-# output: robot_poseトピック(geometry_msgs/msg/Pose型)
+# input: odometry_poseトピック(geometry_msgs/msg/PoseStamped型)
+# output: robot_poseトピック(geometry_msgs/msg/PoseStamped型)
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 
 class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('localize_node')
         self.subscription = self.create_subscription(
-            Pose,
+            PoseStamped,
             'odometry_pose',
             self.listener_callback,
             10)
         self.subscription
 
         self.publisher_ = self.create_publisher(
-            Pose, 
+            PoseStamped, 
             'robot_pose',
             10)
-        self.odom_pose = Pose()
+        self.odom_pose = PoseStamped()
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
-    def listener_callback(self, msg: Pose):
+    def listener_callback(self, msg: PoseStamped):
         self.odom_pose = msg
 
     def timer_callback(self):
--- odometry_calculator/src/odometry_node.cpp
+++ odometry_calculator/src/odometry_node.cpp
@@ -21,6 +21,9 @@ void OdomPubSub::_topic_callback(const nucleo_agent::msg::OdometerData::SharedPt
     auto _pose = odom.get_pose();
     tf2::Quaternion q;
     q.setRPY(0, 0, _pose.theta);  // ロール、ピッチは0、ヨーを設定
+      // Headerの設定
+    msg_pose->header.stamp = rclcpp::Clock().now();
+    msg_pose->header.frame_id = "map";  // フレームIDの設定
     msg_pose->pose.position.x = _pose.x;
     msg_pose->pose.position.y = _pose.y;
     msg_pose->pose.position.z = 0.0;
