580df63a128aed9c5ba751886b087e4ced068a2c
tonto2423 <tnt.kosen2423@gmail.com>
2023-12-15 12:02:18 +0900

    fix: PI制御器を動くように変えた．

--- calc_vel/calc_vel/PI_controller_class.py
+++ calc_vel/calc_vel/PI_controller_class.py
@@ -1,23 +1,41 @@
 # description: PI制御を行うクラス
 
-import numpy as np
+class PIController:
+    def __init__(self, kp: float, ki: float, max_input: float):
+        self.kp = kp
+        self.ki = ki
+        self.max_input = max_input
+        self.integral = 0
+        self.last_error = 0
 
-class PI_controller:
-    def __init__(self) -> None:
-        self.Kp = 0.0
-        self.Ki = 0.0
-        self.dt = 0.0
-        self.error = 0.0
-        self.error_sum = 0.0
-        self.output = 0.0
+    def update(self, target_position: float, current_position: float, dt: float) -> float:
+        error = target_position - current_position
+        self.integral += error * dt  # 積分計算にdtを考慮
 
-    def set_params(self, Kp, Ki, dt):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.dt = dt
+        # 入力飽和のチェックと調整
+        if self.calculate_input(error) > self.max_input or self.calculate_input(error) < -self.max_input:
+            self.integral -= error * dt
 
-    def calc_output(self, error):
-        self.error = error
-        self.error_sum += error * self.dt
-        self.output = self.Kp * self.error + self.Ki * self.error_sum
-        return self.output
+        return self.calculate_input(error)
+
+    def calculate_input(self, error: float) -> float:
+        p_control = self.kp * error
+        i_control = self.ki * self.integral
+        control_input = p_control + i_control
+        control_input = min(control_input, self.max_input)
+        control_input = max(control_input, -self.max_input)
+
+        return control_input
+
+    def set_gains(self, kp: float, ki: float):
+        """
+        ゲインの設定を更新するメソッド
+        """
+        self.kp = kp
+        self.ki = ki
+
+    def reset_integral(self):
+        """
+        誤差の積分をリセットするメソッド
+        """
+        self.integral = 0
