41ce2e41b3c9d6b3ec8029f8ffc8b34eca1ad65b
tonto2423 <tnt.kosen2423@gmail.com>
2023-12-16 00:33:21 +0900

    fix: launchファイルを使えるようにフォルダパスを追加した．

--- calc_vel/setup.py
+++ calc_vel/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('./launch/*.launch.xml')),
+        (os.path.join('share', package_name), glob('./launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
