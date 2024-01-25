73efa0b97725263835cc30af976fe0407b468daf
tonto2423 <tnt.kosen2423@gmail.com>
2024-01-25 14:39:59 +0900

    feat: recieve mecha_state

--- pure_pursuit/pure_pursuit/pure_pursuit_node.py
+++ pure_pursuit/pure_pursuit/pure_pursuit_node.py
@@ -5,41 +5,43 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, Twist
 from std_msgs.msg import Bool
 from nav_msgs.msg import Path
+from mecha_control.msg import PointAndMechaStateArray
 
-class VelocityCalculator(Node):
+class PurePursuitNode(Node):
     def __init__(self):
         super().__init__('pure_pursuit_node')
         self.subscription = self.create_subscription(
             PoseStamped, '/robot_pose', self.pose_callback, 10)
         self.publisher = self.create_publisher(Twist, '/robot_vel', 10)
+        # Pathメッセージをパブリッシュするためのパブリッシャーの初期化
+        self.path_publisher = self.create_publisher(Path, '/robot_path', 10)
+
         # 動作制御のサブスクリプション
         self.subscription_control = self.create_subscription(
             Bool, '/robot_control', self.control_callback, 10)
-        
         self.is_active = False  # 初期状態では停止
+
+        self.path_points_subscriber = self.create_subscription(
+            PointAndMechaStateArray, '/point_command', self.path_points_callback, 10)
 
         # パラメータの宣言
         # この行を変更
-        # self.declare_parameter('on_way_points', [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0]])
         self.declare_parameter('speed', 2.0)
         self.declare_parameter('lookahead_distance', 0.3)
 
         # パラメータの取得
-        # self.on_way_points: list[float] = self.get_parameter('on_way_points').value
-        # self.on_way_points: list[float] = [[0.0, 0.0], [0.0, 1.0], [1.2, 1.0], [1.2, 2.0]]
-        self.on_way_points: list[float] = [[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 2.0], [0.0, 2.0], [0.0, 0.0]]
+        self.on_way_points: list[float] = [[0.0, 0.0], [0.0, 1.0], [1.2, 1.0], [1.2, 2.0]]
         self.speed: float = self.get_parameter('speed').value
         self.lookahead_distance: float = self.get_parameter('lookahead_distance').value
 
         # メンバ変数
         self.path_points: np.ndarray = np.array([])
         self.tangents: np.ndarray = np.array([])
+        self.lookahead_point: np.ndarray = np.array([0.0, 0.0])
+        self.current_path_index: int = -1 # 現在の経路のインデックス
 
         # 経路データと接ベクトルの生成
-        self.generate_path_data(self.on_way_points)
-        # Pathメッセージをパブリッシュするためのパブリッシャーの初期化
-        self.path_publisher = self.create_publisher(Path, '/robot_path', 10)
-        # 経路データのパブリッシュ
+        self.path_points, self.tangents = self.generate_path_data(self.on_way_points)
         self.publish_path(self.path_points)
 
         self.get_logger().info("pure_pursuit_node has been started")
@@ -56,10 +58,10 @@ class VelocityCalculator(Node):
             theta = self.get_yaw(msg)
 
             # 先行点の計算
-            lookahead_point = self.calculate_lookahead_point(self.path_points, self.tangents, current_point)
+            self.lookahead_point = self.calculate_lookahead_point(self.path_points, self.tangents, current_point, self.lookahead_point)
 
             # 速度ベクトルの計算
-            velocity_vector = self.calculate_velocity_vector(lookahead_point, current_point)
+            velocity_vector = self.calculate_velocity_vector(self.lookahead_point, current_point)
 
             # velocity_vectorがNoneでないことを確認してからpublish
             if velocity_vector is not None:
@@ -86,14 +88,21 @@ class VelocityCalculator(Node):
             self.publisher.publish(twist_msg)
         self.publish_path(self.path_points)
 
-    def generate_path_data(self, on_way_points: list[float]) -> None:
+    def path_points_callback(self, msg: PointAndMechaStateArray):
+        for point in msg.points:
+            self.on_way_points.append([point.x, point.y])
+        self.path_points, self.tangents = self.generate_path_data(self.on_way_points)
+        self.publish_path(self.path_points)
+        self.get_logger().info("path_points_callback")
+
+    def generate_path_data(self, on_way_points: list[float]) -> tuple[np.ndarray, np.ndarray]:
         # on_way_points から NumPy 配列に変換
         path_points = np.array(on_way_points)
 
         # 経路と接ベクトルを生成
         path_x = []
         path_y = []
-        self.tangents = []
+        tangents = []
 
         for i in range(len(path_points) - 1):
             start_point = path_points[i]
@@ -108,15 +117,9 @@ class VelocityCalculator(Node):
             # 接ベクトル（各辺に沿ったベクトル）を生成
             tangent = (end_point - start_point) / np.linalg.norm(end_point - start_point)
             for _ in range(20):
-                self.tangents.append(tangent)
+                tangents.append(tangent)
 
-        # 最後の点を追加して経路を閉じる
-        path_x.append(path_points[-1][0])
-        path_y.append(path_points[-1][1])
-        self.tangents.append(self.tangents[0])  # 最初の接ベクトルを最後にも追加
-
-        # 経路データを NumPy 配列に変換して格納
-        self.path_points = np.array([path_x, path_y]).T
+        return np.array([path_x, path_y]).T, tangents
 
     def publish_path(self, path_points):
         # 経路データをPathメッセージに変換
@@ -146,22 +149,25 @@ class VelocityCalculator(Node):
         closest_tangent = None
         min_distance = float('inf')
 
-        for path_point, tangent in zip(path_points, tangents):
+        for i in range(self.current_path_index, len(path_points)):
+            path_point = path_points[i]
+            tangent = tangents[i]
             to_path_vector = path_point - current_point
             distance_to_path = np.linalg.norm(to_path_vector)
 
-            # ロボットの現在位置からの距離がルックアヘッド距離より短い場合、スキップ
-            if distance_to_path > self.lookahead_distance:
+            if distance_to_path < self.lookahead_distance:
                 continue
 
-            # 現在位置からwaypointへのベクトルとwaypointの接ベクトルとの内積
+            # ロボットの現在位置からの距離がルックアヘッド距離より短い場合、スキップ
             dot_product = np.dot(to_path_vector, tangent)
 
-            # 内積が正で、かつ最短距離であれば更新
+            # 内積が正で、かつ最短距離でかつ一つ前の先行点よりもインデックスが大きい点であれば更新
             if dot_product > 0 and distance_to_path < min_distance:
+            # and i > self.current_path_index:
                 min_distance = distance_to_path
                 closest_point = path_point
                 closest_tangent = tangent
+                self.current_path_index = i
 
         # まだ先行点が見つかっていない場合、最も近い点を使用
         if closest_point is None:
@@ -202,7 +208,7 @@ class VelocityCalculator(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    velocity_calculator = VelocityCalculator()
+    velocity_calculator = PurePursuitNode()
     rclpy.spin(velocity_calculator)
     velocity_calculator.destroy_node()
     rclpy.shutdown()
