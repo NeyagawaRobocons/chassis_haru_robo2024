94ce3a836d3424a7c4e8c270f08d9fb76469507a
tonto2423 <tnt.kosen2423@gmail.com>
2024-02-28 03:38:02 +0900

    update msg, action: rewrite `PointAndAngle` to `Pose2DWithSpeed`

--- pure_pursuit/pure_pursuit/action_client.py
+++ pure_pursuit/pure_pursuit/action_client.py
@@ -3,14 +3,16 @@ import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 from nav_msgs.msg import Path
-from geometry_msgs.msg import Point, Pose, PoseStamped
+from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
 from pure_pursuit.action import PathAndFeedback
-from pure_pursuit.msg import PointAndAngle
+from pure_pursuit.msg import Pose2DWithSpeed
 from mecha_actions_class import DaizaCmdActionClient, HinaCmdActionClient
 from std_srvs.srv import SetBool
+import numpy as np
 import pandas as pd
 import os
 from ament_index_python.packages import get_package_share_directory
+from tf_transformations import quaternion_from_euler
 
 class PathActionClient(Node):
     def __init__(self):
@@ -18,35 +20,60 @@ class PathActionClient(Node):
         self._action_client = ActionClient(self, PathAndFeedback, 'path_and_feedback')
         self._daiza_cmd_action_client = DaizaCmdActionClient()
         self._hina_cmd_action_client = HinaCmdActionClient()
-        self._bonbori_cmd_service = self.create_client(SetBool, '/bonbori_cmd')
+        self._bonbori_cmd_service = self.create_client(SetBool, '/set_bonbori')
         self._path_pub = self.create_publisher(Path, '/robot_path', 10)
-        self.get_logger().info('Path action client has been initialized')
+
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('path_number', 1),
+            ]
+        )
+        self.path_number: int = self.get_parameter('path_number').value
+        self.path_file_name: str = f'path{self.path_number}.csv'
+        self.indices_file_name: str = f'indices_and_commands{self.path_number}.csv'
+
         self.path = self.get_path()
-        self.indices, self.daiza_commands, self.hina_commands, self.bonbori_commands = self.get_indices_and_commands()
+        self.indices, self.daiza_commands, self.hina_commands, self.bonbori_commands, self.set_pose_index = self.get_indices_and_commands()
+        self.pre_feedback = -1
         self.get_logger().info('Path and indices have been loaded')
+        self.get_logger().info(f"path_number: {self.path_number}")
+        self.get_logger().info(f"path_file_name: {self.path_file_name}")
+        self.get_logger().info(f"indices_file_name: {self.indices_file_name}")
         # self.get_logger().info(f"Path: {self.path}")
+        self.get_logger().info(f"first point of path: {self.path[0]}")
         self.get_logger().info(f"Indices: {self.indices}")
+        self.get_logger().info(f"first commands: {self.daiza_commands[0]}, {self.hina_commands[0]}, {self.bonbori_commands[0]}")
         self.get_logger().info(f"Daiza commands: {self.daiza_commands}")
         self.get_logger().info(f"Hina commands: {self.hina_commands}")
         self.get_logger().info(f"Bonbori commands: {self.bonbori_commands}")
+        self.get_logger().info(f"Set pose index: {self.set_pose_index}")
         self.send_goal(self.path, self.indices)
         self.get_logger().info('Goal has been sent')
 
-    def send_goal(self, path: PointAndAngle, indices: list[int]) -> None:
+        self.get_logger().info('Path action client has been initialized')
+
+    def send_goal(self, path: Pose2DWithSpeed, indices: list[int]) -> None:
         path_msg = Path()
         path_msg.header.frame_id = 'map'
-        path_msg.poses = [PoseStamped(pose=Pose(position=Point(x=point.x, y=point.y))) for point in path]
+        path_msg.poses = [PoseStamped(pose=Pose(position=Point(x=point.x, y=point.y), orientation=self.yaw_to_quaternion(point.theta + np.pi/2))) for point in path]
         self._path_pub.publish(path_msg)
         self.get_logger().info('Path has been published')
 
         goal_msg = PathAndFeedback.Goal()
         goal_msg.path.path = path
         goal_msg.feedback_indices = [int(index) for index in indices]
+        goal_msg.set_pose_index = self.set_pose_index
 
         self._action_client.wait_for_server()
         self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
         self._send_goal_future.add_done_callback(self.goal_response_callback)
         self.get_logger().info('Goal has been sent')
+
+    def yaw_to_quaternion (self, yaw: float) -> Quaternion:
+        q_raw = quaternion_from_euler(0, 0, yaw)
+        q = Quaternion(x=q_raw[0], y=q_raw[1], z=q_raw[2], w=q_raw[3])
+        return q
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
@@ -60,18 +87,27 @@ class PathActionClient(Node):
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.final_index))
+        result = future.result().result.final_index
+        self.get_logger().info('Result: {0}'.format(result))
+        if result in self.indices:
+            index = self.indices.index(result)
+            daiza_state: int = int(self.daiza_commands[index])
+            hina_state: int = int(self.hina_commands[index])
+            bonbori_state: bool = bool(self.bonbori_commands[index])
+            self.send_mecha_commands(daiza_state, hina_state, bonbori_state)
 
     def feedback_callback(self, feedback_msg):
         feedback: int = feedback_msg.feedback.current_index
         self.get_logger().info('Received feedback: {0}'.format(feedback))
+        if feedback == self.pre_feedback: # 連続して同じfeedbackが送られてくることがあるため
+            return
         if feedback in self.indices:
             index = self.indices.index(feedback)
             daiza_state: int = int(self.daiza_commands[index])
             hina_state: int = int(self.hina_commands[index])
             bonbori_state: bool = bool(self.bonbori_commands[index])
             self.send_mecha_commands(daiza_state, hina_state, bonbori_state)
+        self.pre_feedback = feedback
 
     def send_mecha_commands(self, daiza_state: int, hina_state: int, bonbori_state: bool) -> None:
         self.get_logger().info(f"daiza_state: {daiza_state}, hina_state: {hina_state}, bonbori_state: {bonbori_state}")
@@ -84,9 +120,9 @@ class PathActionClient(Node):
 
     def get_path(self):
         path = []
-        df = pd.read_csv(os.path.join(get_package_share_directory('pure_pursuit'), 'csv', 'path.csv'))
+        df = pd.read_csv(os.path.join(get_package_share_directory('pure_pursuit'), 'csv', self.path_file_name))
         for i in range(len(df)):
-            point = PointAndAngle()
+            point = Pose2DWithSpeed()
             point.x = df.at[i, 'x']
             point.y = df.at[i, 'y']
             point.theta = df.at[i, 'theta']
@@ -98,13 +134,17 @@ class PathActionClient(Node):
         daiza_commands = []
         hina_commands = []
         bonbori_commands = []
-        df = pd.read_csv(os.path.join(get_package_share_directory('pure_pursuit'), 'csv', 'indices_and_commands.csv'))
+        set_pose_index: int = 0
+        df = pd.read_csv(os.path.join(get_package_share_directory('pure_pursuit'), 'csv', self.indices_file_name))
         for i in range(len(df)):
             indices.append(df.at[i, 'index'])
             daiza_commands.append(df.at[i, 'command1'])
             hina_commands.append(df.at[i, 'command2'])
             bonbori_commands.append(df.at[i, 'command3'])
-        return indices, daiza_commands, hina_commands, bonbori_commands
+            if df.at[i, 'set_pose_flag']:
+                set_pose_index = int(df.at[i, 'index'])
+
+        return indices, daiza_commands, hina_commands, bonbori_commands, set_pose_index
 
 def main(args=None):
     rclpy.init(args=args)
